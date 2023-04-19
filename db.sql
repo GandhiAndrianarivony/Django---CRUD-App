f16c700e0f0e0002f410874b8c4520852139cb32
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2023-04-19 10:33:40 +0300

    dev environment

---
+++ db.sql
@@ -0,0 +1,19 @@
+-- Create database
+CREATE DATABASE blog;
+
+-- Create user which is assigned to this previous database i.e
+CREATE USER blogadmin WITH ENCRYPTED PASSWORD 'django_password';
+
+-- 
+ALTER ROLE blogadmin SET client_encoding TO 'utf8';
+ALTER ROLE blogadmin SET default_transaction_isolation TO 'read committed';
+
+-- Grant blog db access to blogadmin
+GRANT ALL PRIVILEGES ON DATABASE blog TO blogadmin;
+
+CREATE TABLE user(
+    id BIGINT SERIAL,
+    name VARCHAR (150),
+    age INTEGER
+
+)
